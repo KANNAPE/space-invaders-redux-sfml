f4ecc9c396748f5c88d2a903e4a768c72dc8604f
KANNAPE <bergerthomas38@gmail.com>
2026-03-18 23:18:46 +0100

    added assets directory, with first font asset + implementation of basic state machine with a small uninteractible menu

--- SpaceInvadersRedux/Sources/Game/Game.cpp
+++ SpaceInvadersRedux/Sources/Game/Game.cpp
@@ -13,6 +13,8 @@ void Game::RunGame()
 {
 	sf::Clock clock;
 
+	m_states.push_back(State::E_Menu);
+
 	while (m_sfWindow.isOpen())
 	{
 		float deltaTime = clock.restart().asSeconds();
@@ -34,18 +36,68 @@ void Game::ProcessEvents()
 			m_sfWindow.close();
 		}
 	}
+
+	switch (m_states.back())
+	{
+	case State::E_Menu:
+		ProcessEvents_Menu();
+		break;
+	case State::E_Settings:
+		//ProcessEvents_Settings();
+		break;
+	case State::E_Game:
+		///ProcessEvents_Game();
+		break;
+	case State::E_Pause:
+		//ProcessEvents_Pause();
+		break;
+	default:
+	{}
+	}
 }
 
 void Game::Update(float _deltaTime)
 {
-	m_player.Update(_deltaTime);
+	switch (m_states.back())
+	{
+	case State::E_Menu:
+		Update_Menu(_deltaTime);
+		break;
+	case State::E_Settings:
+		//Update_Settings(_deltaTime);
+		break;
+	case State::E_Game:
+		//Update_Game(_deltaTime);
+		break;
+	case State::E_Pause:
+		//Update_Pause(_deltaTime);
+		break;
+	default:
+	{}
+	}
 }
 
 void Game::Render()
 {
 	m_sfWindow.clear();
 
-	m_player.Render(m_sfWindow);
+	switch (m_states.back())
+	{
+	case State::E_Menu:
+		Render_Menu();
+		break;
+	case State::E_Settings:
+		//Render_Settings();
+		break;
+	case State::E_Game:
+		//Render_Game();
+		break;
+	case State::E_Pause:
+		//Render_Pause();
+		break;
+	default:
+	{}
+	}
 
 	m_sfWindow.display();
 }
